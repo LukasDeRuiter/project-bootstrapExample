c624dd03f884397c39c4b4067fd81522d66a8c4d
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-25 16:33:41 +0200

    added calculator to js for shop

--- shop.js
+++ shop.js
@@ -1,4 +1,14 @@
 document.getElementById("submitBtn").addEventListener('click', function(){
-    let clayPrice = document.getElementById("amountOfClay").value * 0.5;
+    let clayPrice = document.getElementById("amountOfClay").value * 0.50;
     let copperPrice = document.getElementById("amountOfCopper").value * 2.75;
+    let tinPrice = document.getElementById("amountOfTin").value * 6.50;
+    let ironPrice = document.getElementById("amountOfIron").value * 5.00;
+    let coalPrice = document.getElementById("amountOfCoal").value * 2.50;
+    let goldPrice = document.getElementById("amountOfGold").value * 225.00;
+    let mithrilPrice = document.getElementById("amountOfMithril").value * 275.50;
+    let adamantitePrice = document.getElementById("amountOfAdamantite").value * 945.00;
+
+    let totalPrice = clayPrice + copperPrice + tinPrice + ironPrice + coalPrice + goldPrice + mithrilPrice + adamantitePrice;
+
+    document.getElementById("calculatedCost").innerHTML = `total: $${totalPrice}`;
 })
