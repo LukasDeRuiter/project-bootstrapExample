b64aca5bb7df26b7014afe264a8b0ffbac694cc7
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-25 16:17:31 +0200

    finished first part of shop, added shop.js file

---
+++ shop.js
@@ -0,0 +1,4 @@
+document.getElementById("submitBtn").addEventListener('click', function(){
+    let clayPrice = document.getElementById("amountOfClay").value * 0.5;
+    let copperPrice = document.getElementById("amountOfCopper").value * 2.75;
+})
